eb83e48785b1308fbb847b75e6c2d4ae56c4f88f
michelyp <michelypintom@gmail.com>
2024-05-17 11:40:15 +0200

    Add project files.

---
+++ MvcApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesAWS.Models;
+using MvcApiPersonajesAWS.Services;
+
+namespace MvcApiPersonajesAWS.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        private ServiceApiPersonaje service;
+        public PersonajesController(ServiceApiPersonaje service)
+        {
+            this.service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Personaje> personajes = await this.service.GetPersonajesAsync();
+            return View(personajes);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Personaje personaje)
+        {
+            await this.service.CreatePersonajeAsync(personaje.Nombre, personaje.Imagen);
+            return RedirectToAction("Index");
+            
+        }
+    }
+}
---
+++ MvcApiPersonajesAWS/Services/ServiceApiPersonaje.cs
@@ -0,0 +1,56 @@
+﻿using MvcApiPersonajesAWS.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MvcApiPersonajesAWS.Services
+{
+    public class ServiceApiPersonaje
+    {
+        private MediaTypeWithQualityHeaderValue header;
+        private string UrlApi;
+        public ServiceApiPersonaje(IConfiguration configuration)
+        {
+            this.UrlApi = configuration.GetValue<string>("ApiUrls:ApiPersonajes");
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                string request = "api/personajes";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response = await client.GetAsync(this.UrlApi + request);
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Personaje> personajes = await response.Content.ReadAsAsync<List<Personaje>>();
+                    return personajes;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        public async Task CreatePersonajeAsync(string nombre, string imagen)
+        {
+            using(HttpClient client = new HttpClient()){
+                string request = "api/personajes";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                Personaje per = new Personaje
+                {
+                    IdPersonaje = 0,
+                    Nombre = nombre,
+                    Imagen = imagen
+                };
+                string json = JsonConvert.SerializeObject(per);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(this.UrlApi + request, content);
+
+            }
+        }
+        
+    }
+}
